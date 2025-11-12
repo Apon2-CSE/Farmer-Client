0e75c7231dc51bafaed15e1a296896230cdc4f6d
Apon Talukder <apontalukder212@gmail.com>
2025-11-13 02:00:22 +0600

    backend update

--- src/Component/HeroSection.jsx
+++ src/Component/HeroSection.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Slider from "react-slick";
-import slider1 from "../assets/slider1.jpg";
-import slider2 from "../assets/slider2.jpg";
-import slider3 from "../assets/slider3.jpg";
+import slider1 from "../assets/k1.jpg";
+import slider2 from "../assets/k2.jpg";
+import slider3 from "../assets/k3.jpg";
 
 const HeroSection = () => {
   const slides = [
